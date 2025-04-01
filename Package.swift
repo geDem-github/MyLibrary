5f1e7fa8aa0322b9c8de4576b0926b9c9e70c4a3
gdm <harukikoga.dev@gmail.com>
2025-04-01 22:36:51 +0900

    iosも含めた

--- Package.swift
+++ Package.swift
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
     name: "MyLibrary",
     platforms: [
-       .macOS(.v13)
+        .macOS(.v13),
+        .iOS(.v15)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
@@ -15,7 +16,7 @@ let package = Package(
             targets: ["MyLibrary"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.110.1")
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.110.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -23,7 +24,7 @@ let package = Package(
         .target(
             name: "MyLibrary",
             dependencies: [
-                .product(name: "Vapor", package: "vapor")
+                .product(name: "Vapor", package: "vapor"),
             ]
         ),
         
